323ca7eea14a840a85925430d25f848115265752
BGilz7 <B-Gil@msn.com>
2019-02-16 17:56:12 -0500

    added database

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,37 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Email
+from wtforms import StringField, SubmitField, BooleanField, PasswordField, IntegerField
+from flask import flash
+from app.models import User
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
 
 class EmailForm(FlaskForm):
     email = StringField('Email:', validators=[DataRequired(), Email()])
     message = StringField('Message:', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username:', validators=[DataRequired()])
+    password = PasswordField('Password:', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+
+class RegisterForm(FlaskForm):
+    first_name = StringField('First Name:', validators=[DataRequired()])
+    last_name = StringField('Last Name:', validators=[DataRequired()])
+    username = StringField('Username:', validators=[DataRequired()])
+    email = StringField('E-mail:', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Register')
+
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            flash('Sorry but those credentials are already in use')
+            raise ValidationError('Username already taken.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            flash('Sorry but those credentials are already in use')
+            raise ValidationError('Email already taken.')
